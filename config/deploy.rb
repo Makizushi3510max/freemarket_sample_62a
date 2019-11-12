30d90ac5d6d349f76b94e1096e65d4a2d6e8e390
Error4577 <donttreadonme4577@gmail.com>
2019-11-13 00:38:36 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,6 +14,7 @@ set :branch, ENV['BRANCH'] || "master"
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+set :linked_files, fetch(:linked_files, []).push("config/master.key")
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
