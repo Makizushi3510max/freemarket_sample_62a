3eefcf7a32ef5ca1f84ba6d0d63b2d2d2c228010
kktakano <kktakano03@gmail.com>
2019-11-14 21:42:20 +0900

    create-view-files
    ルートの設定
    mypageコントローラー作成
    show,profile,card,identification,logoutのビューファイルの作成

---
+++ app/controllers/mypages_controller.rb
@@ -0,0 +1,28 @@
+class MypagesController < ApplicationController
+
+  def show
+    
+  end
+
+  def profile
+    
+  end
+
+  def card
+    
+  end
+
+  # def card_create
+    
+  # end
+
+  def identification
+    
+  end
+
+  def logout
+    
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'products#index'
+
+
+  resource :mypage, only: :show do
+    member do
+      get 'profile'
+      get 'card'
+      get 'identification'
+      get 'logout'
+    end
+  end
 end
