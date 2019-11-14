4a8adc4f21e9c257cdcc0188cbd2ae6c0dc03d27
Makizushi3510max <mikoto.makizuru3510@icloud.com>
2019-11-14 20:58:23 +0900

    top-page-library

---
+++ app/assets/javascripts/slick.js
@@ -0,0 +1,8 @@
+$(function() {
+  $('.slider-list').slick({
+  autoplaySpeed: 2500,
+  autoplay: true,
+  accessibility: true,
+  dots: true,
+  });
+});
---
+++ app/assets/javascripts/top.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function ($) {
+  $(function() {
+      $('.slide-banner').slick({
+      autoplaySpeed: 2500,
+      autoplay: true,
+      accessibility: true,
+      dots: true,
+      arrows: false,
+      });
+  });
+});
