37efe42e2688b0b94a45be307f44158911b811cd
Error4577 <donttreadonme4577@gmail.com>
2019-11-17 20:52:41 +0900

    create sms authentication form

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -7,15 +7,29 @@ class SignupController < ApplicationController
   end
 
   def registration_validates
-    session = user_params
+    session[:nickname] = user_params[:nickname]
+    session[:email] = user_params[:email]
+    session[:date_of_birth] = Date.new(
+      user_params["date_of_birth(1i)"].to_i,
+      user_params["date_of_birth(2i)"].to_i,
+      user_params["date_of_birth(3i)"].to_i)
+    session[:last_name] = user_params[:last_name]
+    session[:first_name] = user_params[:first_name]
+    session[:last_name_kana] = user_params[:last_name_kana]
+    session[:first_name_kana] = user_params[:first_name_kana]
     # binding.pry
-    redirect_to registration_signup_index_path
+    redirect_to sms_authentication_signup_index_path
+    
   end
 
   def sms_authentication
+    @user = User.new
   end
 
-  def sms_authentication
+  def sms_authentication_validates
+    session[:phone_number] = user_params[:phone_number]
+    binding.pry
+    redirect_to sms_confirmation_signup_index_pathexit
   end
 
   def sms_confirmation
@@ -33,6 +47,6 @@ class SignupController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:nickname, :email, :password, :last_name, :first_name, :last_name_kana, :first_name_kana, :date_of_birth)
+    params.require(:user).permit(:nickname, :email, :password, :last_name, :first_name, :last_name_kana, :first_name_kana, :date_of_birth, :phone_number)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
       get   'sms_authentication'
       post  'sms_authentication'    =>  'signup#sms_authentication_validates'
       get   'sms_confirmation'
+      post  'sms_confirmation'      =>  'signup#sms_confirmation_validates'
       get   'address'
       get   'creditcard'
       get   'done'
