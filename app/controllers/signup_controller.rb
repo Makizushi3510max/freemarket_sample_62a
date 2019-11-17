3ebd471525dc99a69350b19f1304f60e320ea82a
Error4577 <donttreadonme4577@gmail.com>
2019-11-17 19:56:16 +0900

    create_registration

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -3,6 +3,16 @@ class SignupController < ApplicationController
   end
 
   def registration
+    @user = User.new
+  end
+
+  def registration_validates
+    session = user_params
+    # binding.pry
+    redirect_to registration_signup_index_path
+  end
+
+  def sms_authentication
   end
 
   def sms_authentication
@@ -18,5 +28,11 @@ class SignupController < ApplicationController
   end
 
   def done
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:nickname, :email, :password, :last_name, :first_name, :last_name_kana, :first_name_kana, :date_of_birth)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,15 @@ Rails.application.routes.draw do
   # resources :users, only: [:create, :index, :edit, :update]
   resources :signup, only: [:index, :create] do
     collection do
-      get 'login' # マークアップ作業の為の仮置きっすbyZaki
-      get 'registration'
-      get 'sms_authentication'
-      get 'sms_confirmation'
-      get 'address'
-      get 'creditcard'
-      get 'done'
+      get   'login' # マークアップ作業の為の仮置きっすbyZaki
+      get   'registration'
+      post  'registration'          =>  'signup#registration_validates'
+      get   'sms_authentication'
+      post  'sms_authentication'    =>  'signup#sms_authentication_validates'
+      get   'sms_confirmation'
+      get   'address'
+      get   'creditcard'
+      get   'done'
     end
   end
 end
