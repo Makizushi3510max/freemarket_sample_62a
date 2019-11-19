c99855f851c85314708c14632e4ac73e45df60d9
E07ZK <ef22dec@icloud.com>
2019-11-19 21:52:38 +0900

    Update user.rb
    userモデルにomniauthableを追加
    コールバックメソッドfrom_omniauthを定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,16 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: %i[facebook google_oauth2]
   has_many :addresses
   has_one_attached :avatar
+
+  # devise コールバック時にこいつが呼び出される
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+    end
+  end
 end
