d4a5b3b87cf1c40f11076bcc7b93d95f2a0b8437
E07ZK <ef22dec@icloud.com>
2019-11-19 20:44:02 +0900

    add column to user
    userテーブルにoauth用のpoviderカラムとuidカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_18_084751) do
+ActiveRecord::Schema.define(version: 2019_11_19_112054) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -74,6 +74,8 @@ ActiveRecord::Schema.define(version: 2019_11_18_084751) do
     t.string "first_name_kana", null: false
     t.string "last_name_kana", null: false
     t.date "date_of_birth", null: false
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
