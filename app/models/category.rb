6040f8cf64a037a0d58c10661c0cd2fb30c47732
Error4577 <donttreadonme4577@gmail.com>
2019-11-22 17:10:41 +0900

    Update category.rb
    アソシエーションを定義

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,7 @@
 class Category < ApplicationRecord
+  has_many :products
+  has_many :category_brands
+  has_many :brands, through: :category_brands
+  has_many :category_sizes
+  has_many :sizes, through: :category_sizes
 end
