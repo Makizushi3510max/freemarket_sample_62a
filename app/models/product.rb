0bf40fde0f3ad57d12d7e28c981f383603c0bb24
Error4577 <donttreadonme4577@gmail.com>
2019-11-23 16:52:49 +0900

    define association
    アソシエーションを定義

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ApplicationRecord
   belongs_to :seller, class_name: "User", foreign_key: "seller_id"
   belongs_to :buyer, class_name: "User", foreign_key: "buyer_id"
+  belongs_to :category
+  belongs_to :brand
+  belongs_to :size
+  has_many_attached :images
 end
