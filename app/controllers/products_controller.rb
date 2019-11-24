b74dc0564780b324e6c11ee3e562c24cc29db9cc
Error4577 <donttreadonme4577@gmail.com>
2019-11-24 13:31:08 +0900

    create product sell form
    仮で商品出品機能を実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,27 @@ class ProductsController < ApplicationController
   end
 
   def new
+    @product = Product.new
+    @categories = Category.all
+    @sizes = Size.all
+    # @root_categories = Category.roots
+  end
+
+  def create
+    # binding.pry
+    # Product.create(
+    #   name:           product_params[:name],
+    #   description:    product_params[:description],
+    #   condition:      product_params[:condition],
+    #   category_id:    product_params[:category_id],
+    #   size_id:        product_params[:size_id],
+    #   shipping_area:  product_params[:shipping_area],
+    #   shipping_cost:  product_params[:shipping_cost],
+    #   shipping_date:  product_params[:shipping_date],
+    #   price:          product_params[:price]
+    # )
+    Product.create(product_params)
+    redirect_to products_path
   end
 
   def purchase
@@ -10,4 +31,27 @@ class ProductsController < ApplicationController
 
   def done
   end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :condition, :category_id, :size_id, :shipping_cost, :shipping_area, :shipping_date, :price).merge(seller_id: current_user.id)
+  end
+
+  # def set_category
+  #   Category.find_by(id: "#{params.require(:product).permit(:category_id)["category_id"]}")
+  # end
 end
+
+
+# Category.find_by(id: "#{18}")
+# id: 18, name: "Tシャツ/カットソー(半袖/袖なし)", created_at: Sat, 23 Nov 2019 08:07:41 UTC +00:00, updated_at: Sat, 23 Nov 2019 08:07:41 UTC +00:00, ancestry: "2/6">
+# [17] pry(#<ProductsController>)>   ;
+
+# params.require(:product).permit(:category_id).to_h
+# Unpermitted parameters: :name, :description, :condition, :shipping_cost, :shipping_area, :shipping_date, :price
+# => {"category_id"=>"18"}
+
+# hoge = params.require(:product).permit(:category_id).to_h
+# hoge["category_id"]
+# => "18"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,39 @@
+
+# 親カテゴリー
 ladies = Category.create(name: "レディース")
-ladies_child = ladies.children.create(name: "トップス")
-ladies_grandchild = ladies_child.children.create(name: "Tシャツ/カットソー(半袖/袖なし)")
+mens = Category.create(name: "メンズ")
+
+# 子カテゴリー
+ladies_child1 = ladies.children.create(name: "トップス")
+ladies_child2 = ladies.children.create(name: "ジャケット/アウター")
+ladies_child3 = ladies.children.create(name: "パンツ")
+
+mens_child1 = mens.children.create(name: "トップス")
+mens_child2 = mens.children.create(name: "ジャケット/アウター")
+mens_child3 = mens.children.create(name: "パンツ")
+
+# 孫カテゴリー
+ladies_grandchild1 = ladies_child1.children.create(name: "Tシャツ/カットソー(半袖/袖なし)")
+ladies_grandchild2 = ladies_child1.children.create(name: "Tシャツ/カットソー(七分/長袖)")
+ladies_grandchild3 = ladies_child1.children.create(name: "シャツ/ブラウス(半袖/袖なし)")
+
+ladies_grandchild4 = ladies_child2.children.create(name: "テーラードジャケット")
+ladies_grandchild5 = ladies_child2.children.create(name: "ノーカラージャケット")
+ladies_grandchild6 = ladies_child2.children.create(name: "Gジャン/デニムジャケット")
+
+ladies_grandchild7 = ladies_child3.children.create(name: "デニム/ジーンズ")
+ladies_grandchild8 = ladies_child3.children.create(name: "ショートパンツ")
+ladies_grandchild9 = ladies_child3.children.create(name: "カジュアルパンツ")
+
+
+mens_grandchild1 = mens_child1.children.create(name: "Tシャツ/カットソー(半袖/袖なし)")
+mens_grandchild2 = mens_child1.children.create(name: "Tシャツ/カットソー(七分/長袖)")
+mens_grandchild3 = mens_child1.children.create(name: "シャツ")
+
+mens_grandchild4 = mens_child2.children.create(name: "テーラードジャケット")
+mens_grandchild5 = mens_child2.children.create(name: "ノーカラージャケット")
+mens_grandchild6 = mens_child2.children.create(name: "Gジャン/デニムジャケット")
+
+mens_grandchild7 = mens_child3.children.create(name: "デニム/ジーンズ")
+mens_grandchild8 = mens_child3.children.create(name: "ワークパンツ/カーゴパンツ")
+mens_grandchild9 = mens_child3.children.create(name: "スラックス")
