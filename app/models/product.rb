1910e5ab163fd8c7244ad4021a6be9267055515b
Error4577 <donttreadonme4577@gmail.com>
2019-11-24 13:30:32 +0900

    Update product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,8 @@
 class Product < ApplicationRecord
   belongs_to :seller, class_name: "User", foreign_key: "seller_id"
-  belongs_to :buyer, class_name: "User", foreign_key: "buyer_id"
+  belongs_to :buyer, class_name: "User", foreign_key: "buyer_id", optional: true
   belongs_to :category
-  belongs_to :brand
+  belongs_to :brand, optional: true
   belongs_to :size
   has_many_attached :images
 end
