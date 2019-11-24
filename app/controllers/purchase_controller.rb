7c4144e2fb91cc0ed2c2a5596bea1bac41849ce6
kktakano <kktakano03@gmail.com>
2019-11-24 15:57:39 +0900

    logout_function_implementation
    ログアウトボタンに機能実装
    ログイン機能修正

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -3,6 +3,8 @@ class PurchaseController < ApplicationController
   require 'payjp'
 
   def index
+    # cardテーブルからpayjpの顧客idを検索し取得
+    card = Card.where(user_id: current_user.id).first
     
   end
 
