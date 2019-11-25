05205e8d82bd08ea129054822000377e5f626f03
E07ZK <ef22dec@icloud.com>
2019-11-25 20:55:37 +0900

    Enable to display prefecture name
    ActiveHashから値を取得し都道府県名を表示する

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,7 @@ class Product < ApplicationRecord
   belongs_to :brand, optional: true
   belongs_to :size
   has_many_attached :images
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture, class_name: "Prefecture", foreign_key: "shipping_area"
 end
