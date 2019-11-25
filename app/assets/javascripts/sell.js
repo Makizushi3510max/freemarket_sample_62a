952908fb0e8ac121c49f8a05d604954ce9b30a12
Error4577 <donttreadonme4577@gmail.com>
2019-11-25 15:47:50 +0900

    Update sell.js
    コメント削除とリファクタリング

--- app/assets/javascripts/sell.js
+++ app/assets/javascripts/sell.js
@@ -1,41 +1,43 @@
 $(function(){
+  // 画像のプレビューを表示させる機能
   $(document).on("change", "#product_images",function(e){
     var file = $(this).prop('files')
     console.log(file);
-    // console.log("ほげほげ")
-    // var input_file = $("#product_images");
-    // console.log(input_file[0]);
-    // console.log(input_file)
-    // var items = input_file[0].files;
-    // console.log(items[0])
-    // var image = `<img class= "lower-message__image" src="${message.image_url}">`;
   })
 
 
 
-  var buildSelectBox_Children = `
-  <div>
-    <div class='select-wrap'>
-      <i class="fa fa-chevron-down"></i>
-      <select class="select-default" name="product[category_id]" id="product_category_id">
-      <option value="">---</option>
-    </div>
-  </div>`
 
-  function buildSelectBox_Children_Options(child){
-    var optionHtml =`
-    <option value="${child.id}">
-      ${child.name}
-    </option>
-  `
-  return optionHtml;
+  // カテゴリー選択後に、対応するセレクトボックスを表示させる機能
+  function buildSelectBox_Children(category_children){
+    // 子カテゴリーのセレクトボックスを生成
+    function buildSelectBox_Children_Options(child){
+      var optionHtml =`
+        <option value="${child.id}">
+          ${child.name}
+        </option>`
+    return optionHtml;
+    }
+
+    var buildSelectBox_Children = `
+      <div>
+        <div class='select-wrap' id="wrap-child">
+          <i class="fa fa-chevron-down"></i>
+          <select class="select-default" name="product[child_category_id]" id="product_child_category_id">
+          <option value="">---</option>
+        </div>
+      </div>`
+
+    $(".form-group.category").append(buildSelectBox_Children)
+
+    category_children.forEach(function(child) {
+      $("#product_child_category_id").append(buildSelectBox_Children_Options(child))
+    });
   };
 
-
+  // id: "product_root_category_id"のセレクトボックスが選択されると発火
   $(document).on("change", "#product_root_category_id",function(e){
-    // console.log("ほげ")
     var selected_category_root = $('#product_root_category_id').val();
-    console.log(selected_category_root)
 
     $.ajax({
       url: "/products/get_category_children",
@@ -43,36 +45,31 @@ $(function(){
       data: { root_category_id: selected_category_root },
       dataType: "json"
     })
-    .done(function(data){
-      console.log(data);
-      $(".form-group.category").append(buildSelectBox_Children)
-      data.forEach(function( child ) {
-        console.log(child)
-        $("#product_category_id").append(buildSelectBox_Children_Options(child))
-      });
-      // $("#product_category_id").append(buildSelectBox_Children_Options(data))
-      // console.log();
-      // console.log(category_children)
+    .done(function(category_children){
+      $("#wrap-child").remove()
+      buildSelectBox_Children(category_children)
+    })
+    .fail(function(){
+      console.log('error');
+    })
+  })
 
+  // id: "product_child_category_id"のセレクトボックスが選択されると発火
+  $(document).on("change", "#product_child_category_id",function(e){
+    var selected_category_root = $('#product_child_category_id').val();
+
+    $.ajax({
+      url: "/products/get_category_grandchildren",
+      type: "GET",
+      data: { child_category_id: selected_category_child },
+      dataType: "json"
+    })
+    .done(function(category_children){
+      $("#wrap-child").remove()
+      buildSelectBox_Children(category_children)
     })
     .fail(function(){
       console.log('error');
     })
   })
 })
-
-// var image = message.image_url ? `<img class= "lower-message__image" src="${message.image_url}">` : "" ;
-
-// $(document).on("change", "#file_photo", function(e) {
-//   var reader;
-//   if (e.target.files.length) {
-//     reader = new FileReader;
-//     reader.onload = function(e) {
-//       var userThumbnail;
-//       userThumbnail = document.getElementById('thumbnail');
-//       $("#userImgPreview").addClass("is-active");
-//       userThumbnail.setAttribute('src', e.target.result);
-//     };
-//     return reader.readAsDataURL(e.target.files[0]);
-//   }
-// });
