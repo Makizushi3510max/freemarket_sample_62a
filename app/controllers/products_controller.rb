676d64548f0b25872f90034a15d9392794b07489
Makizushi3510max <mikoto.makizuru3510@icloud.com>
2019-11-27 18:52:23 +0900

    商品一覧

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,18 @@
 class ProductsController < ApplicationController
   def index
-    # @products = Product.includes(:user).page(params[:page]).per(5).order("created_at DESC")
     @products = Product.all.order("id DESC").limit(10)
+    lady = Category.find(1)
+    mens = Category.find(417)
+    @appliance = Category.find(1181)
+    @toy = Category.find(946)
+
+    @ladys_category = Product.where(category_id: lady.id)
+    # binding.pry
+    @mens_category = Product.where(category_id: mens.id)
   end
 
   def show
+
   end
   
   def new
@@ -28,8 +36,20 @@ class ProductsController < ApplicationController
   end
 
   def create
-    binding.pry
-    Product.create(product_params)
+    # Product.create(product_params)
+    @product = Product.create(
+      name:             product_params[:name],
+      description:      product_params[:description],
+      condition:        product_params[:condition],
+      category_id:      product_params[:grandchild_category_id],
+      size_id:          product_params[:size_id],
+      shipping_cost:    product_params[:shipping_cost],
+      shipping_area:    product_params[:shipping_area],
+      shipping_date:    product_params[:shipping_date],
+      price:            product_params[:price],
+      seller_id:        product_params[:seller_id],
+      images:           product_params[:images]
+    )
     redirect_to products_path
   end
 
@@ -42,6 +62,8 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:name, :description, :condition, :category_id, :size_id, :shipping_cost, :shipping_area, :shipping_date, :price, images: []).merge(seller_id: current_user.id)
+    # params.require(:product).permit(:name, :description, :condition, :category_id, :size_id, :shipping_cost, :shipping_area, :shipping_date, :price, images: []).merge(seller_id: current_user.id)
+
+    params.require(:product).permit(:name, :description, :condition, :grandchild_category_id, :size_id, :shipping_cost, :shipping_area, :shipping_date, :price, images: []).merge(seller_id: current_user.id)
   end
 end
