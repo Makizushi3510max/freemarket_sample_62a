9320d2b45bd005ec01f69e0b4e17ff1e7526a9c4
Error4577 <donttreadonme4577@gmail.com>
2019-11-30 07:59:03 +0900

    Update recaptcha.rb

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,7 +1,4 @@
-# config/initializers/recaptcha.rb 
-Recaptcha.configure do |config|   
+Recaptcha.configure do |config|
   config.site_key  = Rails.application.credentials.recaptcha[:site_key]
-  config.secret_key = Rails.application.credentials.recaptcha[:secret_key]  
-# Uncomment the following line if you are using a proxy server:   
-# config.proxy = 'http://myproxy.com.au:8080' 
+  config.secret_key = Rails.application.credentials.recaptcha[:secret_key]
 end
