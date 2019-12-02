21b8dc9509d950bfeae782fe222f9e4f30f5ba61
Error4577 <donttreadonme4577@gmail.com>
2019-12-02 17:41:32 +0900

    Update sell.js
    画像のプレビュー削除

--- app/assets/javascripts/sell.js
+++ app/assets/javascripts/sell.js
@@ -29,8 +29,6 @@ $(function(){
                     </ul>
                   </div>`
 
-  // var dropzone = 
-
   // プレビューを描画する関数
   function renderingPreview(images,preview_count){
     $.each(images, function(index,value){
@@ -40,16 +38,28 @@ $(function(){
         // 1~4枚目の画像までは普通にプレビューを表示
         if (0 <= preview_count && preview_count <= 3 ){
           $("#images1").append(buildImagePreviewBox(preview_count,src))
-          $("#preview-upper").removeClass("have-item-" + preview_count)
+          // $("#preview-upper").removeClass("have-item-" + preview_count)
+          $("#preview-upper").removeClass (function (index, className) {
+            return (className.match (/have-item-\d/g) || []).join(' ');
+          });
           $("#preview-upper").addClass("have-item-" + (preview_count + 1))
-          $(".sell-upload-drop-label").removeClass("have-item-" + preview_count)
+          // $(".sell-upload-drop-label").removeClass("have-item-" + preview_count)
+          $(".sell-upload-drop-label").removeClass (function (index, className) {
+            return (className.match (/have-item-\d/g) || []).join(' ');
+          });
           $(".sell-upload-drop-label").addClass("have-item-" + (preview_count + 1))
         // 5枚目の画像が存在した場合、drop_boxはhave-item-0に戻る
         } else if (preview_count == 4) {
           $("#images1").append(buildImagePreviewBox(preview_count,src))
-          $("#preview-upper").removeClass("have-item-4")
+          // $("#preview-upper").removeClass("have-item-4")
+          $("#preview-upper").removeClass (function (index, className) {
+            return (className.match (/have-item-\d/g) || []).join(' ');
+          });
           $("#preview-upper").addClass("have-item-5")
-          $(".sell-upload-drop-label").removeClass("have-item-4")
+          // $(".sell-upload-drop-label").removeClass("have-item-4")
+          $(".sell-upload-drop-label").removeClass (function (index, className) {
+            return (className.match (/have-item-\d/g) || []).join(' ');
+          });
           $(".sell-upload-drop-label").addClass("have-item-0")
         // 6~9枚目の画像は新しいulタグ内に挿入する。
         } else if (5 <= preview_count && preview_count <= 8) {
@@ -57,16 +67,28 @@ $(function(){
             $(".sell-upload-items-container").append(images2)
           }
           $("#images2").append(buildImagePreviewBox(preview_count,src))
-          $("#preview-lower").removeClass("have-item-" + (preview_count - 5))
+          // $("#preview-lower").removeClass("have-item-" + (preview_count - 5))
+          $("#preview-lower").removeClass (function (index, className) {
+            return (className.match (/have-item-\d/g) || []).join(' ');
+          });
           $("#preview-lower").addClass("have-item-" + ((preview_count - 5) + 1))
-          $(".sell-upload-drop-label").removeClass("have-item-" + (preview_count - 5))
+          // $(".sell-upload-drop-label").removeClass("have-item-" + (preview_count - 5))
+          $(".sell-upload-drop-label").removeClass (function (index, className) {
+            return (className.match (/have-item-\d/g) || []).join(' ');
+          });
           $(".sell-upload-drop-label").addClass("have-item-" + ((preview_count - 5) + 1))
         // 10枚目の画像が存在した場合、drop_boxは消える
         } else {
           $("#images2").append(buildImagePreviewBox(preview_count,src))
-          $("#preview-lower").removeClass("have-item-4")
+          // $("#preview-lower").removeClass("have-item-4")
+          $("#preview-lower").removeClass (function (index, className) {
+            return (className.match (/have-item-\d/g) || []).join(' ');
+          });
           $("#preview-lower").addClass("have-item-5")
-          $(".sell-upload-drop-label").removeClass("have-item-4")
+          // $(".sell-upload-drop-label").removeClass("have-item-4")
+          $(".sell-upload-drop-label").removeClass (function (index, className) {
+            return (className.match (/have-item-\d/g) || []).join(' ');
+          });
           $(".sell-upload-drop-label").addClass("have-item-5")
         }
         preview_count += 1
@@ -95,16 +117,44 @@ $(function(){
     // 削除ボタンが押されたら配列の何番目なのかを取得する
     target_image = Number($(this).attr("id").split("-")[2])
 
-    // 選択された画像以降全てのプレビューを削除
+    // 削除ボタンが押された画像以降全てのプレビューを一度削除
     for(var i = target_image; i < 10; i++){
-      // console.log($("#image-" + i))
       $("#image-" + i).remove()
     }
 
-    // 配列から画像を削除する
+    // 削除ボタンが押された画像が配列の最後尾だった場合
+    if(target_image == images.length - 1){
+      flag = true
+    }
+
+    // 配列から削除ボタンが押された画像を削除する
     images.splice(target_image,1)
-    // 削除された画像の以降の画像を描画しなおす
+
+    // 削除された画像以降の画像を描画しなおす
     preview_count = renderingPreview(images,target_image)
+
+    if(flag == true){
+      // console.log("発火")
+      if(images.length <= 5){
+        $("#preview-upper").removeClass (function (index, className) {
+          return (className.match (/have-item-\d/g) || []).join(' ');
+        });
+        $("#preview-upper").addClass("have-item-" + preview_count)
+        $(".sell-upload-drop-label").removeClass (function (index, className) {
+          return (className.match (/have-item-\d/g) || []).join(' ');
+        });
+        $(".sell-upload-drop-label").addClass("have-item-" + preview_count)
+      } else if(5 <= images.length) {
+        $("#preview-lower").removeClass (function (index, className) {
+          return (className.match (/have-item-\d/g) || []).join(' ');
+        });
+        $("#preview-lower").addClass("have-item-" + (preview_count - 5))
+        $(".sell-upload-drop-label").removeClass (function (index, className) {
+          return (className.match (/have-item-\d/g) || []).join(' ');
+        });
+        $(".sell-upload-drop-label").addClass("have-item-" + (preview_count - 5))
+      }
+    }
   })
 
 
