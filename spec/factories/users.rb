95167c674dd399729c06acbdfa8a7a49990816d3
kktakano <kktakano03@gmail.com>
2019-12-02 20:24:01 +0900

    test_invalid_without_a_nickname
    ニックネームのテストしました

---
+++ spec/factories/users.rb
@@ -0,0 +1,15 @@
+FactoryBot.define do
+
+  factory :user do
+    nickname              {"abe"}
+    email                 {"kkk@gmail.com"}
+    password              {"00000000"}
+    last_name             {"あべ"}
+    first_name            {"ひろし"}
+    last_name_kana        {"アベ"}
+    first_name_kana       {"ヒロシ"}
+    date_of_birth         {"2000-2-20"}
+    phone_number          {"11122223333"}
+  end
+
+end
