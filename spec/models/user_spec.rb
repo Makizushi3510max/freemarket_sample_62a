9f81b3aab1de2a8a6d747700d422df8ca9c10318
kktakano <kktakano03@gmail.com>
2019-12-03 16:46:53 +0900

    Update user_spec.rb
    重複したemailが存在する場合登録できないこと
    passwordが7文字以上であれば登録できること
    passwordが6文字以下であれば登録できないこと

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -71,5 +71,24 @@ describe User do
       user = build(:user, nickname: "aaaaaaaaaaaaaaaaaaaa")
       expect(user).to be_valid
     end
+    # 重複したemailが存在する場合登録できないこと
+    it "is invalid with a duplicate email address" do
+      user = create(:user)
+      another_user = build(:user, email: user.email)
+      another_user.valid?
+      expect(another_user.errors[:email]).to include("has already been taken")
+    end
+    # passwordが7文字以上であれば登録できること
+    it "is valid with a password that has more than 7 characters " do
+      user = build(:user, password: "0000aaa")
+      user.valid?
+      expect(user).to be_valid
+    end
+    # passwordが6文字以下であれば登録できないこと
+    it "is invalid with a password that has less than 6 characters " do
+      user = build(:user, password: "000aaa")
+      user.valid?
+      expect(user.errors[:password]).to include("is too short (minimum is 7 characters)")
+    end
   end
 end
