090993b4b9f3d26a2b6f80c9184471f5a1bd4928
kktakano <kktakano03@gmail.com>
2019-12-04 19:18:29 +0900

    mypage_html_haml_lower_side_edit
    購入中と過去の取引のタブ切り替え

--- app/assets/javascripts/mypage.js
+++ app/assets/javascripts/mypage.js
@@ -1,11 +1,20 @@
 $(function() {
    let tabs = $(".tab");
-   function tabSwitch() {
+   function tabSwitch1() {
      $(".tab").removeClass("active");
      $(this).addClass("active");
      const index = tabs.index(this);
      $(".mypage-item-list").removeClass("show").eq(index).addClass("show");
    }
+   tabs.click(tabSwitch1);
 
-   tabs.click(tabSwitch);
+   let mypagetabs = $(".mypagetab");
+   function tabSwitch2() {
+     $(".mypagetab").removeClass("active");
+     $(this).addClass("active");
+     const index = tabs.index(this);
+     $(".mypage-tab-transaction").removeClass("active").eq(index).addClass("active");
+   }
+   mypagetabs.click(tabSwitch2);
+
 })
