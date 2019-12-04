d11613ef94171ff71d2486d30b092dd22516d370
kktakano <kktakano03@gmail.com>
2019-12-04 17:45:46 +0900

    mypage_html_haml_upper_side_edit_
    ’お知らせ’と’やることリスト’のjq化

--- app/assets/javascripts/mypage.js
+++ app/assets/javascripts/mypage.js
@@ -1,5 +1,11 @@
 $(function() {
-  $('.tab li').click(function() {
-    
-  })
+   let tabs = $(".tab");
+   function tabSwitch() {
+     $(".tab").removeClass("active");
+     $(this).addClass("active");
+     const index = tabs.index(this);
+     $(".mypage-item-list").removeClass("show").eq(index).addClass("show");
+   }
+
+   tabs.click(tabSwitch);
 })
