4426c70c13137c1ce8ce2d0b55a6def84ebb9a9b
kktakano <kktakano03@gmail.com>
2019-12-10 01:59:30 +0900

    pulldown_menu_edit

--- app/assets/javascripts/pulldown.js
+++ app/assets/javascripts/pulldown.js
@@ -1,4 +1,49 @@
 // $(function(){
+
+//   function buildCategoryList_Root(category_roots){
+//     category_roots.forEach(function(root){
+//       $(".product-category__root").append(buildCategoryBox_Root(root));
+//     });
+
+//     function buildCategoryBox_Root(root){
+//       var optionHtml = `<li id="product_root_category_id" value="${root.id}">
+//                           ${root.name}
+//                           <ul></ul>
+//                         </li>`
+//       return optionHtml;
+//     };
+//   };
+
+//   function buildCategoryList_Children(category_children){
+//     category_children.forEach(function(children) {
+//       $(".product-category__children").append(buildCategoryBox_children(children));
+//     });
+
+//     function buildCategoryBox_children(children){
+//       var optionHtml = `<li id="product_children_category_id" value="${children.id}">
+//                           ${children.name}
+//                         </li>`
+//       return optionHtml;
+//     };
+//   };
+
+//   function buildCategoryList_GrandChildren(category_grandchildren){
+//     category_grandchildren.forEach(function(grandchildren) {
+//       $(".product-category__grandchildren").append(buildCategoryBox_grandchildren(grandchildren));
+//     });
+
+//     function buildCategoryBox_grandchildren(grandchildren){
+//       var optionHtml = `<li id="product_grandchildren_category_id" value="${grandchildren.id}">
+//                           ${grandchildren.name}
+//                         </li>`
+//       return optionHtml;
+//     };
+//   };
+
+
+
+
+
 //   //header__content__categoryにホバーしたら発火
 //   $(document).on("mouseover",".category__select", function(e) {
 //     $.ajax({
@@ -15,21 +60,6 @@
 //     })
 
 //     // カテゴリー選択後に、対応するリストを表示させる関数
-//     function buildCategoryList_Root(category_roots){
-//       category_roots.forEach(function(root){
-//         //header__content__categoryの下に生成したhtmlを追加
-//         $(".product-category__root").append(buildCategoryBox_Root(root));
-//       });
-
-//       //ルートカテゴリーのリストを生成
-//       function buildCategoryBox_Root(root){
-//         var optionHtml = `<li id="product_root_category_id" value="${root.id}">
-//                             ${root.name}
-//                             <ul></ul>
-//                           </li>`
-//         return optionHtml;
-//       };
-//     };
 //   });
 
 
@@ -37,6 +67,7 @@
 //     $(".product-category__grandchildren").empty();
 //     $(".product-category__children").empty();
 //     $("#product_root_category_id.active").removeClass("active");
+//     $(".product-category__children").addClass("white");
 
 //     var selected_category_root = $(this).val();
 //     $("#product_root_category_id").removeClass("active");
@@ -56,26 +87,13 @@
 //       console.log('error');
 //     })
 
-//     function buildCategoryList_Children(category_children){
-//       category_children.forEach(function(children) {
-//         //header__content__categoryの下に生成したhtmlを追加
-//         $(".product-category__children").append(buildCategoryBox_children(children));
-//       });
-
-//       //ルートカテゴリーのリストを生成
-//       function buildCategoryBox_children(children){
-//         var optionHtml = `<li id="product_children_category_id" value="${children.id}">
-//                             ${children.name}
-//                           </li>`
-//         return optionHtml;
-//       };
-//     };
 //   });
 
 
 //   $(document).on("mouseover","#product_children_category_id", function(e) {
 //     $(".product-category__grandchildren").empty();
 //     $("#product_children_category_id.active").removeClass("active");
+//     $(".product-category__grandchildren").addClass("white");
 
 //     var selected_category_children = $(this).val();
 //     $("#product_children_category_id").removeClass("active");
@@ -89,26 +107,11 @@
 //     })
 //     .done(function(category_grandchildren){
 //       //ajax成功時ルートカテゴリのリストを表示する関数を呼び出す
-//       buildCategoryList_Children(category_grandchildren);
+//       buildCategoryList_GrandChildren(category_grandchildren);
 //     })
 //     .fail(function(){
 //       console.log('error');
 //     })
-
-//     function buildCategoryList_Children(category_grandchildren){
-//       category_grandchildren.forEach(function(grandchildren) {
-//         //header__content__categoryの下に生成したhtmlを追加
-//         $(".product-category__grandchildren").append(buildCategoryBox_grandchildren(grandchildren));
-//       });
-
-//       //ルートカテゴリーのリストを生成
-//       function buildCategoryBox_grandchildren(grandchildren){
-//         var optionHtml = `<li id="product_grandchildren_category_id" value="${grandchildren.id}">
-//                             ${grandchildren.name}
-//                           </li>`
-//         return optionHtml;
-//       };
-//     };
 //   })
 
 
@@ -117,9 +120,11 @@
 //     $(this).addClass("active");
 //   }).on("mouseleave",".product-category__grandchildren", function(e){
 //     $(".product-category__grandchildren").empty();
+//     $(".product-category__grandchildren").removeClass("white");
 //   });
 
 //   $(document).on("mouseleave",".product-category", function(e) {
+//     $(".product-category__children").removeClass("white");
 //     $(".product-category div").empty();
 //   });
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,27 +4,14 @@ class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
   before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
-
+  before_action :set_categories
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar])
   end
 
   def set_categories
-    @categories = Category.where(params[:id])
-    @category_children1 = Category.where(parent_id: 1)
-    @category_children2 = Category.where(parent_id: 2)
-    @category_children3 = Category.where(parent_id: 3)
-    @category_children4 = Category.where(parent_id: 4)
-    @category_children5 = Category.where(parent_id: 5)
-    @category_children6 = Category.where(parent_id: 6)
-    @category_children7 = Category.where(parent_id: 7)
-    @category_children8 = Category.where(parent_id: 8)
-    @category_children9 = Category.where(parent_id: 9)
-    @category_children10 = Category.where(parent_id: 10)
-    @category_children11 = Category.where(parent_id: 11)
-    @category_children12 = Category.where(parent_id: 12)
-    @category_children13 = Category.where(parent_id: 13)
+    @parents = Category.all.order("id ASC").limit(13)
   end
   
   private
